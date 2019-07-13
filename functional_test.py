a1c3a9b517152e0651ca545f2be67d31a5385689
MilenaLee <leemihyun90@gmail.com>
2019-07-13 17:09:06 +0900

    1-4] 템플릿을 이용하도록 홈페이지 뷰를 리팩터링

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 chromedriver_dir = r"C:\Users\USER\Desktop\django\django-tdd-study\chromedriver.exe"
@@ -12,10 +13,42 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # 미현이는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
+        # 해당 웹 사이트를 확인하러 간다
         self.browser.get("http://localhost:8000")
 
+        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 그녀는 바로 작업을 추가하리고 한다
+        inputbox = self.browser.find_element_by_id("id_new_item")
+
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "작업 아이템 입력"
+        )
+
+        # "방탄소년단 콘서트 가기"라고 텍스트 상자에 입력한다
+        # (미현이는 아미이다ㅎㅎ)
+        inputbox.send_keys("방탄소년단 콘서트 가기")
+
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
+        # "1: 방탄소년단 콘서트 가기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: 방탄소년단 콘서트 가기" for row in rows),
+        )
+
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
+        # 다시 "방탄소년단 콘서트에서 굿즈 구입하기"라고 입력한다. (다시 말하지만 미현이는 아미이다!)
         self.fail("Finish the test!")
+
+        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
 
 
 if __name__ == "__main__":
