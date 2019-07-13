0170bc384b025b153ea92d89c965e840c5e21ceb
MilenaLee <leemihyun90@gmail.com>
2019-07-13 21:11:34 +0900

    1-5] 아이템용 모델과 마이그레이션 연계

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,6 +6,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def remove_csrf(self, origin):
@@ -39,3 +40,21 @@ class HomePageTest(TestCase):
         )
         response_html = self.remove_csrf(response.content.decode())
         self.assertEqual(response_html, expected_html)
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "첫 번째 아이템"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "두 번째 아이템"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "첫 번째 아이템")
+        self.assertEqual(second_saved_item.text, "두 번째 아이템")
