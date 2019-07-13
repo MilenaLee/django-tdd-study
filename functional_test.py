6544a120dad73b1d98dff5a81707848201e3666b
MilenaLee <leemihyun90@gmail.com>
2019-07-13 19:03:28 +0900

    1-5] 기능 테스트 리팩터

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 미현이는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
@@ -37,6 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 방탄소년단 콘서트 가기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table("1: 방탄소년단 콘서트 가기")
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "방탄소년단 콘서트에서 굿즈 구입하기"라고 입력한다. (다시 말하지만 미현이는 아미이다!)
@@ -45,10 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: 방탄소년단 콘서트 가기", [row.text for row in rows])
-        self.assertIn("2: 방탄소년단 콘서트에서 굿즈 구입하기", [row.text for row in rows])
+        self.check_for_row_in_list_table("2: 방탄소년단 콘서트에서 굿즈 구입하기")
+        self.check_for_row_in_list_table("1: 방탄소년단 콘서트 가기")
 
         # 미현이는 사이트가 입력한 목록을 잘 저장하는지 궁금하다
         # 사이트는 그녀를 위한 특정 url을 생성해준다
