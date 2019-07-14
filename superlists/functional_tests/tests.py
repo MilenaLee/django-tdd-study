20fd8582f4de85b42661280a948c7fa7dbaedaa3
MilenaLee <leemihyun90@gmail.com>
2019-07-14 22:43:46 +0900

    1-6] 새로운 사용자 시나리오를 기능 테스트에 추가

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -43,6 +43,8 @@ class NewVisitorTest(LiveServerTestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 방탄소년단 콘서트 가기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
+        mihyun_list_url = self.browser.current_url
+        self.assertRegex(mihyun_list_url, '/lists/.+')
         self.check_for_row_in_list_table("1: 방탄소년단 콘서트 가기")
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
@@ -55,7 +57,37 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table("2: 방탄소년단 콘서트에서 굿즈 구입하기")
         self.check_for_row_in_list_table("1: 방탄소년단 콘서트 가기")
 
-        # 미현이는 사이트가 입력한 목록을 잘 저장하는지 궁금하다
-        # 사이트는 그녀를 위한 특정 url을 생성해준다
-        # 이때 URL에 대한 설명도 함께 제공된다
+        # 새로운 사용자인 태형이가 사이트에 접속한다
+
+        ## 새로운 브라우저 세션을 이용해서 미현이의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다
+        self.browser.quit()
+        browser = webdriver.Chrome(chromedriver_dir)
+
+        # 태형이가 홈페이지에서 접속한다
+        # 미현의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("방탄소년단 콘서트 가기", page_text)
+        self.assertNotIn("방탄소년단 콘서트에서 굿즈 구입하기", page_text)
+
+        # 태형이가 새로운 작업 아이템을 입력하기 시작한다.
+        # 태형이는 아이돌이다ㅎㅎ
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("신곡 작업")
+        inputbox.send_keys(keys.ENTER)
+
+        # 태형이가 전용 URL을 취득한다.
+        v_list_url = self.browser.current_url
+        self.assertRegex(v_list_url, '/lists/.+')
+        self.assertNotEqual(v_list_url, mihyun_list_url)
+
+        # 미현이가 입력한 흔적이 없다는 것을 다시 확인한다
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("방탄소년단 콘서트 가기")
+        self.assertNotIn("방탄소년단 콘서트에서 굿즈 구입하기")
+
+        # 둘 다 만족하고 잠자리에 든다
+
+
         self.fail("Finish the test!")
