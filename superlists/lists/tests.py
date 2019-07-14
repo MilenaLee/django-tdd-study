e303ebecc301be6facd3599e3b5bbdf75471685a
MilenaLee <leemihyun90@gmail.com>
2019-07-15 00:23:34 +0900

    1-6] List 모델 클래스 추가 및 관련 기능 테스트 추가

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,7 +6,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 def remove_csrf(origin):
@@ -29,15 +29,22 @@ class HomePageTest(TestCase):
         self.assertEqual(response_decode, expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "첫 번째 아이템"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "두 번째 아이템"
+        second_item.list = list_
         second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -45,7 +52,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "첫 번째 아이템")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "두 번째 아이템")
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -54,8 +63,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_displays_all_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        list_ = List.objects.create()
+        Item.objects.create(text="itemey 1", list=list_)
+        Item.objects.create(text="itemey 2", list=list_)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
