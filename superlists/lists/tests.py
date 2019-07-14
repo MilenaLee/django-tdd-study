978c8d71c5a6891f0f94e0c0171be603dfe15f62
MilenaLee <leemihyun90@gmail.com>
2019-07-14 23:55:31 +0900

    1-6] 두 개 폼을 위한 URL 적용, views와 tests에 있는 코드 이동, 신규 URL

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,7 +15,6 @@ def remove_csrf(origin):
 
 
 class HomePageTest(TestCase):
-
     def test_root_url_resolvers_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
@@ -28,32 +27,6 @@ class HomePageTest(TestCase):
         )
         response_decode = remove_csrf(response.content.decode())
         self.assertEqual(response_decode, expected_html)
-
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = "POST"
-        request.POST['item_text'] = "신규 작업 아이템"
-
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "신규 작업 아이템")
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = "POST"
-        request.POST['item_text'] = "신규 작업 아이템"
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
@@ -78,7 +51,6 @@ class ItemModelTest(TestCase):
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
-        print(response)
         self.assertTemplateUsed(response, "list.html")
 
     def test_displays_all_items(self):
@@ -89,3 +61,19 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            "/lists/new",
+            data={"item_text": "신규 작업 아이템"}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "신규 작업 아이템")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            "/lists/new",
+            data={"item_text": "신규 작업 아이템"}
+        )
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from lists.views import home_page, view_list
+from lists.views import home_page, view_list, new_list
 
 urlpatterns = [
     path("", home_page, name="home"),
     path("lists/the-only-list-in-the-world/", view_list, name="view_list"),
+    path("lists/new", new_list, name="new_list"),
     path('admin/', admin.site.urls),
 ]
