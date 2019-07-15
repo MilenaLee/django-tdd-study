fa9aaf2fee0370b33af0c43f95335bd36006abae
MilenaLee <mmhyun.lee@gmail.com>
2019-07-15 23:32:53 +0900

    2-7] 레이아웃과 스타일링을 위한 FT 첫 단계

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -89,4 +89,23 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 둘 다 만족하고 잠자리에 든다
 
-        self.fail("Finish the test!")
+    def test_layout_and_styling(self):
+        # 미현이는 메인 페이지를 방문한다
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 그녀는 입력 상자가 가운데 배치된 것을 본다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        inputbox.send_keys('testing\n')
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
